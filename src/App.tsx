da721d94ebf1ac7cdef16a9b05aaa20912dd97d9
PBERGBUSCH <bergbusch.wp@triton-partners.com>
2023-06-07 17:50:28 +0200

    make buttons work

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 /*
  * imports of react packages
  */
-import React from "react"
+import React, {useState} from "react"
 
 /*
  * imports of implemented components and functions
  */
-import NavigationBar from "./components/NavigationBar"
-import BottomBar from "./components/BottomBar"
+import LogoImage from "./assets/images/npa_dark.PNG"
 
 /*
  * imports of styles
@@ -16,38 +15,248 @@ import BottomBar from "./components/BottomBar"
 import "./App.scss"
 
 const App = () => {
+  const [mainVisible, setMainVisible] = useState<boolean>(true)
+  const [pluginsVisible, setPluginsVisible] = useState<boolean>(false)
+  const [appsVisible, setAppsVisible] = useState<boolean>(false)
+  const [mixesVisible, setMixesVisible] = useState<boolean>(false)
+  const [contactVisible, setContactVisible] = useState<boolean>(false)
+  const [impressumVisible, setImpressumVisible] = useState<boolean>(false)
   
   return (
     <div className="flexColumn" style={{backgroundColor: "black"}}
       // prevent the default browser right-click behaviour
       onContextMenu={(e) => {e.preventDefault()}}
     >
-      <NavigationBar />
       <div
-        className="landingPageTitle"
+        className="flexRowCenter navigationBarContainer"
       >
-        Nonphysical Audio
+        <div className="flexRow">
+          <img src={LogoImage} className="logoImage"></img>
+          <div
+            className="button navigationBarText"
+            onClick = {() => {
+              setMainVisible(true)
+              setPluginsVisible(false)
+              setAppsVisible(false)
+              setMixesVisible(false)
+              setContactVisible(false)
+              setImpressumVisible(false)
+            }}
+          >
+              Nonphysical Audio
+          </div>
+        </div>
+        <div style={{width: "60%"}}></div>
+        <div
+          className="button navigationBarText"
+          onClick = {() => {
+            setMainVisible(false)
+            setPluginsVisible(true)
+            setAppsVisible(false)
+            setMixesVisible(false)
+            setContactVisible(false)
+            setImpressumVisible(false)
+          }}
+        >
+          Plugins
+        </div>
+        <div
+          className="button navigationBarText"
+          onClick = {() => {
+            setMainVisible(false)
+            setPluginsVisible(false)
+            setAppsVisible(true)
+            setMixesVisible(false)
+            setContactVisible(false)
+            setImpressumVisible(false)
+          }}
+        >
+          Apps
+        </div>
+        <div
+          className="button navigationBarText"
+          onClick = {() => {
+            setMainVisible(false)
+            setPluginsVisible(false)
+            setAppsVisible(false)
+            setMixesVisible(true)
+            setContactVisible(false)
+            setImpressumVisible(false)
+          }}
+        >
+          Mixes
+        </div>
       </div>
-      <p></p>
-      <div
-        className="landingPageText"
-      >
-        We are a team of scientists and engineers fascinated by the physics of sound
-        and the psychology of music.
+      {mainVisible && (
+        <div className="pageContainer">
+          <div
+            className="pageTitle"
+          >
+            Nonphysical Audio
+          </div>
+          <div className="break"></div>
+          <div
+            className="pageText"
+          >
+              We are a team of scientists and engineers fascinated by the physics of sound
+              and the psychology of music.
+          </div>
+          <div className="break"></div>
+          <div
+            className="pageText"
+          >
+            Our right and left brains are fully connected.
+          </div>
+          <div className="break"></div>
+          <div
+            className="pageText"
+          >
+            We build non-linear models and non-physical solutions for audio synthesis and transformation.
+          </div>
+        </div>
+      )}
+      {pluginsVisible && (
+        <div className="pageContainer">
+          <div
+            className="pageTitle"
+          >
+            Plugins
+          </div>
+          <div className="break"></div>
+          <div
+            className="pageText"
+          >
+              Too many plugins have too many parameters with weird names that no one undertands.
+          </div>
+          <div className="break"></div>
+          <div
+            className="pageText"
+          >
+            Our plugins have at most 3 parameters and we use them ourselves in our mixes.
+          </div>
+          <div className="break"></div>
+          <div
+            className="pageText"
+          >
+            COMING SOON!
+          </div>
+        </div>
+      )}
+      {appsVisible && (
+        <div className="pageContainer">
+          <div
+            className="pageTitle"
+          >
+            Apps
+          </div>
+          <div className="break"></div>
+          <div
+            className="pageText"
+          >
+              You can find these apps in the Google Play Store and in the Apple Store.
+          </div>
+          <div className="break"></div>
+          <div
+            className="pageText"
+          >
+            COMING SOON!
+          </div>
+        </div>
+      )}
+      {mixesVisible && (
+        <div className="pageContainer">
+          <div
+            className="pageTitle"
+          >
+            Mixes
+          </div>
+          <div className="break"></div>
+          <div
+            className="pageText"
+          >
+              These are remixes of my favourite pipe organ tunes.
+          </div>
+          <div className="break"></div>
+          <div
+            className="pageText"
+          >
+            COMING SOON!
+          </div>
+        </div>
+      )}
+      {contactVisible && (
+        <div className="pageContainer">
+          <div
+            className="pageTitle"
+          >
+            Contact
+          </div>
+          <div className="break"></div>
+          <div
+            className="pageText"
+          >
+            <p>Nonphysical Audio</p>
+            <p>Frankfurt Germany</p>
+          </div>
+        </div>
+      )}
+      {impressumVisible && (
+        <div className="pageContainer">
+          <div
+            className="pageTitle"
+          >
+            Impressum
+          </div>
+          <div className="break"></div>
+          <p
+            className="pageText"
+          >
+              COMING SOON!
+          </p>
+        </div>
+      )}
+      <div className="bottomBarContainer">
+        <div
+          className="flexRow"
+        >
+          <div
+            className="bottomBarText"
+          >
+            Developed in Frankfurt |
+          </div>
+          <div
+            className="button bottomBarText"
+            onClick = {() => {
+              setMainVisible(false)
+              setPluginsVisible(false)
+              setAppsVisible(false)
+              setMixesVisible(false)
+              setContactVisible(true)
+              setImpressumVisible(false)
+            }}
+          >
+            Contact
+          </div>
+          <div
+            className="bottomBarText"
+          >
+            |
+          </div>
+          <div
+            className="button bottomBarText"
+            onClick = {() => {
+              setMainVisible(false)
+              setPluginsVisible(false)
+              setAppsVisible(false)
+              setMixesVisible(false)
+              setContactVisible(false)
+              setImpressumVisible(true)
+            }}
+          >
+            Impressum
+          </div>
+        </div>
       </div>
-      <p></p>
-      <div
-        className="landingPageText"
-      >
-        Our right and left brains are fully connected.
-      </div>
-      <p></p>
-      <div
-        className="landingPageText"
-      >
-        We build non-linear models and non-physical solutions for audio synthesis and transformation.
-      </div>
-      <BottomBar />
     </div>
   )
 }
